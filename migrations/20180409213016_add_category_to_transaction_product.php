1cfab0dafa936452286a6ec48809b0a17274fd29
Zechariah Walden <zech@zewadesign.com>
2018-04-09 17:37:31 -0500

    migration for category column on transaction product

---
+++ migrations/20180409213016_add_category_to_transaction_product.php
@@ -0,0 +1,13 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddCategoryToTransactionProduct extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('TransactionProduct');
+        $table->addColumn('category', 'string')
+            ->save();
+    }
+}
