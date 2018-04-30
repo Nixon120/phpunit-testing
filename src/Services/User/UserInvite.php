d1bf879736b47d54e5e66fa711362f5e2b18972c
admin <pralhad.dev427@gmail.com>
2018-04-30 15:27:24 -0700

    refractor-2

--- src/Services/User/UserInvite.php
+++ src/Services/User/UserInvite.php
@@ -103,11 +103,11 @@ class UserInvite
             'user/invite/invite-email.phtml',
             ['user' => $user]
         );
-        $subject = 'You\'re Invited to join' + $user->getOrganization()->getName() + 'Team';
+
         return new Email(
             $user->getEmailAddress(),
             'csr@alldigitalrewards.com',
-            $subject,
+            'You\'re Invited to join '. $user->getOrganization()->getName().' Team',
             $emailBody
         );
     }
