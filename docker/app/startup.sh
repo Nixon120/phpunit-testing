5d72d8fcbf0f1bb038cf6e2dfe6938bec174d53e
Zechariah Walden <zech@zewadesign.com>
2018-05-18 12:53:25 -0500

    update build to consolidate mysql instances, switch to mariadb

--- docker/app/startup.sh
+++ docker/app/startup.sh
@@ -4,11 +4,14 @@
 
 ## Fix Cron issues
 touch /etc/crontab /etc/cron.*/*
-# Place the environment varialbes in a script for CRON jobs to be able to access them.
+# Place the environment variables in a script for CRON jobs to be able to access them.
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
+# Wait until MySQL Server is ready.
+# while ! nc -w 1 -z $MYSQL_HOST 3306; do sleep 0.1; done;
+
 # Run DB Migrations
-php /var/www/html/vendor/robmorgan/phinx/bin/phinx migrate -c /var/www/html/phinx.php &
+php /var/www/html/vendor/robmorgan/phinx/bin/phinx migrate -c /var/www/html/phinx.php
 
 # Temporary migration for the webhook log mongo migrations
 php /var/www/html/cli/migrate-mongo.php
