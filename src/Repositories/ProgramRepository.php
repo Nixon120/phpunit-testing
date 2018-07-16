57589ad3a2bf324806eaab8efe4964c6f73f3b58
Ashok <m.ashok@hirecoder.com>
2018-07-16 15:54:29 -0400

    Added test

--- src/Repositories/ProgramRepository.php
+++ src/Repositories/ProgramRepository.php
@@ -341,7 +341,7 @@ SQL;
                 Validator::nullType()
             ))
             ->attribute('point', Validator::numeric()->min(1)->setName('Point'))
-            ->attribute('unique_id', Validator::notEmpty()->alnum()->noWhitespace()->setName('Unique Id'))
+            ->attribute('unique_id', Validator::notEmpty()->alnum('_ -')->noWhitespace()->setName('Unique Id'))
             ->attribute('organization_id', Validator::notEmpty()->setName('Organization'))
             ->attribute('deposit_amount', Validator::optional(Validator::numeric()->setName('Deposit')))
             ->attribute(
