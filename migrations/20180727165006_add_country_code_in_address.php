db013010dd44cb146c96c2bb3b4a7c9926b241c1
Zechariah Walden <zech@zewadesign.com>
2018-08-03 11:58:45 -0500

    casing on db table

--- migrations/20180727165006_add_country_code_in_address.php
+++ migrations/20180727165006_add_country_code_in_address.php
@@ -7,7 +7,7 @@ class AddCountryCodeInAddress extends AbstractMigration
 {
     public function change()
     {
-        $table = $this->table('address');
+        $table = $this->table('Address');
         $table->addColumn('country_code', 'string', ['limit' => 2, 'after' => 'country', 'null' => true])
             ->save();
     }
