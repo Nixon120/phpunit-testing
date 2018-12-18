bb7a9166331769968dc49c1e4860a2d3a13333ff
Scott Hancock <scotth@alldigitalrewards.com>
2018-12-18 11:22:07 -0700

    DS-861 - Modified query for Program Report

--- src/Services/Report/ProgramSummary.php
+++ src/Services/Report/ProgramSummary.php
@@ -21,11 +21,11 @@ class ProgramSummary extends AbstractReport
             'Program.start_date as `Program Start Date`' => 'Program Start Date',
             'Program.end_date as `Program End Date`' => 'Program End Date',
             'Program.grace_period as `Grace Period`' => 'Grace Period',
-            'COUNT(Participant.id) as `Participant Count`' => 'Participant Count',
-            'SUM(Adjustment.amount) as `Total Participant Points`' => 'Total Participant Points',
-            'COUNT(`Transaction`.id) as `Transaction Count`' => 'Transaction Count',
-            'COUNT(`Transaction`.id) as `Total Redemptions`' => 'Total Redemptions',
-            'SUM(`Transaction`.total) as `Total Redemption Value`' => 'Total Redemption Value'
+            'COUNT(DISTINCT Participant.id) as `Participant Count`' => 'Participant Count',
+            'SUM(Adjustment.adjustment_total) as `Total Participant Points`' => 'Total Participant Points',
+            'SUM(`Transaction`.transCount) as `Transaction Count`' => 'Transaction Count',
+            'SUM(`Transaction`.transCount) as `Total Redemptions`' => 'Total Redemptions',
+            'SUM(`Transaction`.transaction_total) as `Total Redemption Value`' => 'Total Redemption Value'
         ]);
     }
 
@@ -36,12 +36,21 @@ class ProgramSummary extends AbstractReport
 
         $query = <<<SQL
 SELECT SQL_CALC_FOUND_ROWS {$selection}
-FROM Program
-JOIN Organization ON Program.organization_id = Organization.id 
-LEFT JOIN Participant ON Program.id = Participant.program_id
-LEFT JOIN `Transaction` on Participant.id = `Transaction`.participant_id
-LEFT JOIN Adjustment on `Transaction`.participant_id = Adjustment.participant_id
-WHERE 1=1 AND Adjustment.type = 1 
+FROM Participant
+LEFT JOIN Organization ON Organization.id = Participant.organization_id
+LEFT JOIN Program ON Participant.program_id = Program.id
+LEFT JOIN (
+   SELECT participant_id, SUM(amount) AS adjustment_total
+   FROM Adjustment 
+   WHERE `type` = 1   
+   GROUP BY participant_id   
+   ) Adjustment ON Participant.id = Adjustment.participant_id
+LEFT JOIN (
+   SELECT participant_id, SUM(total) AS transaction_total, COUNT(id) AS transCount
+   FROM `Transaction`
+   GROUP BY participant_id
+   ) `Transaction` ON Participant.id = `Transaction`.participant_id
+WHERE 1=1 
 {$this->getFilter()->getFilterConditionSql()}
 GROUP BY Program.unique_id, Program.name
 SQL;
