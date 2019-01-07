bbe05cd984db71a3589f8e293ad200edadeee628
jmuto <jmuto@alldigitalrewards.com>
2019-01-07 16:34:12 -0500

    DS-828-new remove unused code

--- src/Routes/report.php
+++ src/Routes/report.php
@@ -4,6 +4,5 @@ use \Controllers\Report as Controllers;
 $app->group('/api/report', function () use ($app, $createRoute, $updateRoute) {
     $app->get('/meta', Controllers\RequestReportMetaFields::class);
     $app->get('/data', Controllers\RequestReport::class);
-    $app->get('/publish_sftp/{id}', Controllers\RequestReport::class);
     $app->get('', Controllers\ReportList::class);
 });
