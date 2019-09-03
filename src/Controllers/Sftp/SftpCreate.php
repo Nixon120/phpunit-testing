8be40e213f69bc145f0807844ee22a1860be777d
jhoughtelin <jhoughtelin@alldigitalrewards.com>
2019-09-03 15:48:04 -0700

    SHAR-1059 - Removing DB unique key constraint on the sftp file_path field.

--- src/Controllers/Sftp/SftpCreate.php
+++ src/Controllers/Sftp/SftpCreate.php
@@ -47,8 +47,7 @@ class SftpCreate
         $get = $this->request->getParsedBody();
         $get['user_id'] = $this->factory->getAuthenticatedUser()->getId();
 
-        $saved = $this->factory->getSftpRepository()
-            ->insert($get, true);
+        $saved = $this->factory->getSftpRepository()->insert($get);
 
         $response = $this->response->withStatus(200)
             ->withJson($saved);
