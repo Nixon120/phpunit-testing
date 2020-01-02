f50260b033a2cc9a2b0e9d0cf3be6a1e80622720
Zechariah Walden <zech@zewadesign.com>
2020-01-02 15:42:26 -0600

    DS-1634 - global middleware for logging post, only enabled if environmental set

--- src/Controllers/Participant/Modify.php
+++ src/Controllers/Participant/Modify.php
@@ -6,11 +6,9 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Services\Participant\ServiceFactory;
 use Services\Participant\Participant;
-use Traits\LoggerAwareTrait;
 
 class Modify extends AbstractModifyController
 {
-    use LoggerAwareTrait;
     /**
      * @var Participant
      */
@@ -28,7 +26,6 @@ class Modify extends AbstractModifyController
     public function insert()
     {
         $post = $this->request->getParsedBody()??[];
-        $this->log('POST', $post);
         unset($post['credit']);
         if ($participant = $this->service->insert($post)) {
             $output = new OutputNormalizer($participant);
@@ -42,7 +39,6 @@ class Modify extends AbstractModifyController
     public function update($id)
     {
         $post = $this->request->getParsedBody()??[];
-//        $this->log('PUT', $post);
         unset($post['credit']);
         //@TODO: perhaps we can figure out a clean way to remove unneeded fields without explicitly removing them
         //We don't need this.
@@ -54,17 +50,4 @@ class Modify extends AbstractModifyController
         $errors = $this->service->getErrors();
         return $this->returnFormattedJsonError(400, $errors);
     }
-
-    private function log(string $action, array $data)
-    {
-        if(getenv('LOG_API_REQUEST_BODIES') == 1) {
-            $this->getLogger()->notice(
-                'Participant ' . $action,
-                [
-                    'data' => $data
-                ]
-            );
-        }
-    }
-
 }
---
+++ src/Middleware/LogApiRequestBodiesMiddleware.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Middleware;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Services\Participant\ServiceFactory;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Route;
+use Traits\LoggerAwareTrait;
+
+class LogApiRequestBodiesMiddleware
+{
+    use LoggerAwareTrait;
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var Response
+     */
+    private $response;
+
+    /**
+     * @var ServiceFactory
+     */
+    private $participantService;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->participantService = $this->container->get('participant');
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param Response $response
+     * @param callable|null $next
+     * @return mixed
+     */
+    public function __invoke(
+        ServerRequestInterface $request,
+        Response $response,
+        callable $next = null
+    ) {
+        $this->request = $request;
+        $this->response = $response;
+
+        if(getenv('LOG_API_REQUEST_BODIES') == 1) {
+            $post = $this->request->getParsedBody()??[];
+            $this->getLogger()->notice(
+                'API POST Request Bodies',
+                [
+                    'POST' => $post,
+                    'ROUTE' => $request->getAttribute('routeInfo')['request'][1] ?? null
+                ]
+            );
+        }
+
+        return $next($this->request, $this->response);
+    }
+}
