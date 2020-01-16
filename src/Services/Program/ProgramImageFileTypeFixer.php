bd5ab8ddb01d59b08a5707d0c0d4323a721bb017
jmuto <jmuto@alldigitalrewards.com>
2020-01-16 14:21:51 -0500

    FS-21-add-to-repo change method scope

--- src/Services/Program/ProgramImageFileTypeFixer.php
+++ src/Services/Program/ProgramImageFileTypeFixer.php
@@ -15,36 +15,6 @@ class ProgramImageFileTypeFixer
 
     /**
      * @param string $fileName
-     * @return array
-     * @throws \Exception
-     */
-    public function getFileIfImageFileTypeArrayExists(string $fileName): array
-    {
-        $bucketName = getenv('GOOGLE_CDN_BUCKET');
-        $cdnPath = "https://storage.googleapis.com/$bucketName/layout";
-        $fileName = $cdnPath . '/' . $fileName;
-        $exif = @exif_read_data($fileName, 0, true);
-        $mimeType = 'image/jpeg';
-        if (is_array($exif) === true && isset($exif['FILE']) === true) {
-            $mimeType = $exif['FILE']['MimeType'];
-        }
-
-        $explode = explode('/', $mimeType);
-        $type = $explode[1];
-        $tmpFile = '/tmp/img_file.' . $type;
-
-        if (file_put_contents($tmpFile, file_get_contents($fileName))) {
-            $contents = file_get_contents($tmpFile);
-            unlink($tmpFile);
-        } else {
-            throw new \Exception('CDN File Download failure for ' . $fileName);
-        }
-
-        return [$contents, $type];
-    }
-
-    /**
-     * @param string $fileName
      * @return mixed
      */
     public function getImageType(string $fileName)
@@ -67,6 +37,36 @@ class ProgramImageFileTypeFixer
             ->put($imagePath, $imageData);
 
         return $imagePath;
+    }
+
+    /**
+     * @param string $fileName
+     * @return array
+     * @throws \Exception
+     */
+    private function getFileIfImageFileTypeArrayExists(string $fileName): array
+    {
+        $bucketName = getenv('GOOGLE_CDN_BUCKET');
+        $cdnPath = "https://storage.googleapis.com/$bucketName/layout";
+        $fileName = $cdnPath . '/' . $fileName;
+        $exif = @exif_read_data($fileName, 0, true);
+        $mimeType = 'image/jpeg';
+        if (is_array($exif) === true && isset($exif['FILE']) === true) {
+            $mimeType = $exif['FILE']['MimeType'];
+        }
+
+        $explode = explode('/', $mimeType);
+        $type = $explode[1];
+        $tmpFile = '/tmp/img_file.' . $type;
+
+        if (file_put_contents($tmpFile, file_get_contents($fileName))) {
+            $contents = file_get_contents($tmpFile);
+            unlink($tmpFile);
+        } else {
+            throw new \Exception('CDN File Download failure for ' . $fileName);
+        }
+
+        return [$contents, $type];
     }
 
     /**
