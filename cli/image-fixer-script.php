dd71ccd89904a810af40c93cee95e025b175b326
jmuto <jmuto@alldigitalrewards.com>
2020-01-17 15:15:07 -0500

    DS-1660-update-script fix image fixer script, and seeder

--- cli/image-fixer-script.php
+++ cli/image-fixer-script.php
@@ -3,6 +3,10 @@
 use AllDigitalRewards\RewardStack\Services\Program\ProgramImageFileTypeFixer;
 use Psr\Container\ContainerInterface;
 
+//324cdd53610a7f0214d82b0cbc20eb228ae67f67.Array
+//77cfa314c5194c3d41a8d22fcbf327d6f90a96b4.Array
+//dfdbb033770d22bc9cba86a2f1d3ce473922b6fb.Array
+
 /**
  * @var ContainerInterface $container
  */
@@ -12,7 +16,12 @@ require __DIR__ . "/../cli-bootstrap.php";
 $pdo = $container->get('database');
 $fixerService = new ProgramImageFileTypeFixer;
 
-$sql = "SELECT id, program_id, priority FROM `LayoutRow`";
+$sql = "SELECT lc.id, l.program_id, l.priority as row_priority, lc.priority, row_id, image
+FROM layoutrowcard lc
+LEFT JOIN layoutrow l ON lc.row_id = l.id
+WHERE lc.image LIKE '%.Array' 
+  AND l.program_id IS NOT NULL";
+
 $sth = $pdo->prepare($sql);
 $sth->execute();
 $rows = $sth->fetchAll();
@@ -21,17 +30,11 @@ if (!$rows) {
 }
 try {
     foreach ($rows as $row) {
-        $sql = "SELECT priority, row_id, image  FROM `LayoutRowCard` WHERE row_id = {$row['id']}";
+        $imagePath = $row['program_id'] . $row['row_priority'] . $row['priority'];
+        $imageName = $fixerService->resaveCorruptedImageFile($imagePath, $row['image']);
+        $sql = "UPDATE `LayoutRowCard` SET image = ?, updated_at = NOW() WHERE id = {$row['id']}";
         $sth = $pdo->prepare($sql);
-        $sth->execute();
-        $cardRow = $sth->fetch();
-        if (empty($cardRow['image']) === false && $fixerService->getImageType($cardRow['image']) === 'Array') {
-            $imagePath = $row['program_id'] . $row['priority'] . $cardRow['priority'];
-            $imageName = $fixerService->resaveCorruptedImageFile($imagePath, $cardRow['image']);
-            $sql = "UPDATE `LayoutRowCard` SET image = ?, updated_at = NOW() WHERE row_id = {$row['id']}";
-            $sth = $pdo->prepare($sql);
-            $sth->execute([$imageName]);
-        }
+        $sth->execute([$imageName]);
     }
 } catch (\Exception $exception) {
     echo $exception->getMessage();
