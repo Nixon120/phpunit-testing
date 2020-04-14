4a9a04750f4f3ccdcf7548ddadc6f5094055d8c1
Zechariah Walden <zech@zewadesign.com>
2020-04-14 13:26:50 -0500

    DS-1583 - add participant to refund response, scrub participant of non-essential/private items

--- src/Controllers/Participant/TransactionRefund.php
+++ src/Controllers/Participant/TransactionRefund.php
@@ -4,6 +4,7 @@ namespace Controllers\Participant;
 
 use Entities\User;
 use Services\Authentication\Authenticate;
+use Services\Participant\Participant;
 use Slim\Container;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -29,6 +30,11 @@ class TransactionRefund
      * @var \Services\Participant\Transaction
      */
     private $transactionService;
+
+    /**
+     * @var Participant
+     */
+    private $participantService;
 
     public function __construct(Container $container)
     {
@@ -78,6 +84,25 @@ class TransactionRefund
         $this->transactionService = $transactionService;
     }
 
+    /**
+     * @return Participant
+     */
+    public function getParticipantService(): Participant
+    {
+        if($this->participantService === null) {
+            $this->participantService = $this->getContainer()->get('participant')->getService();
+        }
+        return $this->participantService;
+    }
+
+    /**
+     * @param Participant $participantService
+     */
+    public function setParticipantService(Participant $participantService): void
+    {
+        $this->participantService = $participantService;
+    }
+
     public function __invoke(Request $request, Response $response, array $args)
     {
         $this->request = $request;
@@ -105,14 +130,38 @@ class TransactionRefund
             return $this->response = $this->response->withJson(['Resource does not exist'], 404);
         }
 
+        $transaction = $this->transactionService->getTransactionRepository()->getTransaction($refund->getTransactionId());
         $aUser = $refund->getUser()->toArray();
         $aRefund = $refund->toArray();
+        $item = $refund->getItem();
+        $participant = $this->scrubParticipant($this->getParticipantService()->getById($transaction->getId()));
+        unset($item['id']);
+        unset($aUser['password'], $aUser['role'], $aUser['id'], $aUser['invite_token'], $aUser['organization_id']);
+        unset($aRefund['user_id'], $aRefund['transaction_id'], $aRefund['transaction_item_id']);
 
-        unset($aUser['password'], $aUser['invite_token'], $aUser['organization_id']);
-        $aRefund['item'] = $refund->getItem();
+        $aRefund['item'] = $item;
         $aRefund['user'] = $aUser;
-        unset($aRefund['transaction_id'], $aRefund['transaction_item_id']);
+        $aRefund['participant'] = $participant;
         return $this->response = $this->response->withJson($aRefund, 200);
+    }
+
+    private function scrubParticipant(\Entities\Participant $participant)
+    {
+        $program = $participant->getProgram()->getUniqueId();
+        $organization = $participant->getOrganization()->getUniqueId();
+        $address = $participant->getAddress();
+        if(!empty($address)) {
+            $address = $address->toArray();
+            unset($address['participant_id'], $address['id'], $address['reference_id']);
+        }
+        $meta = $participant->getMeta();
+        $participant = $participant->toArray();
+        $participant['program'] = $program;
+        $participant['organization'] = $organization;
+        $participant['address'] = $address;
+        $participant['meta'] = $meta;
+        unset($participant['program_id'], $participant['organization_id'], $participant['sso'], $participant['password'], $participant['id'], $participant['address_reference']);
+        return $participant;
     }
 
     private function issueRefundRequest(array $item)
