e4ed34d8f0ff4fa6f46e7617a3c9806afe04c3fd
Zechariah Walden <zech@zewadesign.com>
2020-04-28 15:00:27 -0500

    DS-1907 - clean up docker app dependencies

--- docker/app/startup.sh
+++ docker/app/startup.sh
@@ -5,14 +5,10 @@
 # Place the environment variables in a script for CRON jobs to be able to access them.
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
-# Wait until MySQL Server is ready.
-# while ! nc -w 1 -z $MYSQL_HOST 3306; do sleep 0.1; done;
+while ! nc -w 1 -z $MYSQL_HOST 3306; do sleep 0.1; done;
 
 # Run DB Migrations
-php /var/www/html/vendor/robmorgan/phinx/bin/phinx migrate -c /var/www/html/phinx.php
+php /app/vendor/robmorgan/phinx/bin/phinx migrate -c /app/phinx.php
 
-# Tail the application log
-tail -f /var/www/html/logs/app.log &
-
-# Supervisord runs Nginx & PHP-FPM Services.
-/usr/bin/supervisord
+/usr/sbin/crond -l 8
+/usr/bin/supervisord -c /etc/supervisord.conf
