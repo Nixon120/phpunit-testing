d0cc9ece8fbb0842ff51025b1ddbe435027cf2c5
Mario Dujmovic <mariodujmovic@Marios-MacBook-Pro.local>
2020-05-06 09:01:32 -0500

    changed sizeof to count

--- tests/integration/API/Reporting/ReportingTest.php
+++ tests/integration/API/Reporting/ReportingTest.php
@@ -9,10 +9,9 @@ class ReportingTest extends AbstractAPITestCase
 
     public function testGetOrganizationsWithNumericLimitParam() //covers appropriate query
     {
-        $this->user = 'admin@alldigitalrewards.com';
         $response = $this->getApiClient()->request(
             'GET',
-            'api/organization?1=1&orderBy[field]=name&orderBy[direction]=asc&limit=4',
+            'api/organization?1=1&orderBy[field]=name&orderBy[direction]=asc&limit=3',
             [
                 'headers' => $this->getHeaders(),
             ]
@@ -20,12 +19,11 @@ class ReportingTest extends AbstractAPITestCase
 
         $decodedResponse = json_decode((string)$response->getBody());
         $this->assertSame(200, $response->getStatusCode());
-        $this->assertSame(4, sizeof($decodedResponse));
+        $this->assertSame(3, count($decodedResponse));
     }
 
     public function testGetOrganizationsWithNonNumericLimitParam() //covers inappropriate query
     {
-        $this->user = 'admin@alldigitalrewards.com';
         $response = $this->getApiClient()->request(
             'GET',
             'api/organization?1=1&orderBy[field]=name&orderBy[direction]=asc&limit=abc',
@@ -38,22 +36,20 @@ class ReportingTest extends AbstractAPITestCase
 
     public function testGetProgramsWithNumericLimitParam() //covers appropriate query
     {
-        $this->user = 'admin@alldigitalrewards.com';
         $response = $this->getApiClient()->request(
             'GET',
-            'api/program?organization=&limit=3&orderBy[field]=name&orderBy[direction]=asc',
+            'api/program?organization=&limit=2&orderBy[field]=name&orderBy[direction]=asc',
             [
                 'headers' => $this->getHeaders(),
             ]
         );
         $decodedResponse = json_decode((string)$response->getBody());
         $this->assertSame(200, $response->getStatusCode());
-        $this->assertSame(3, sizeof($decodedResponse));
+        $this->assertSame(2, count($decodedResponse));
     }
 
     public function testGetProgramsWithNonNumericLimitParam() //covers inappropriate query
     {
-        $this->user = 'admin@alldigitalrewards.com';
         $response = $this->getApiClient()->request(
             'GET',
             'api/program?organization=&limit=abc&orderBy[field]=name&orderBy[direction]=asc',
