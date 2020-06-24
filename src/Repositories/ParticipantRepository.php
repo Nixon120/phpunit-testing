ac889578e7843700b24fbc6647c51dc10ca07d4e
jmuto <jmuto@alldigitalrewards.com>
2020-06-24 13:37:59 -0400

    DS-1994-replace-frozen minor

--- src/Repositories/ParticipantRepository.php
+++ src/Repositories/ParticipantRepository.php
@@ -62,7 +62,7 @@ SELECT
    Participant.deactivated_at, 
    Participant.updated_at,
    Participant.created_at, 
-   IF(Participant.frozen = 1, 'hold', IF(Participant.active = 1, 'active', 'inactive')) as status
+   IF(Participant.frozen = 1, 'hold', IF(Participant.active = 1, 'active', 'inactive')) as `status`
 FROM Participant
 JOIN Organization ON Organization.id = Participant.organization_id
 JOIN Program ON Program.id = Participant.program_id
