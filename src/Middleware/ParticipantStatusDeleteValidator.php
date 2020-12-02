f4521a68273ef5ccbb8cc9069591e4ba6657b7b6
jmuto <jmuto@alldigitalrewards.com>
2020-12-02 17:38:31 -0500

    DS-2268-check-status on update if DATDEl, prevent update

--- src/Middleware/ParticipantStatusDeleteValidator.php
+++ src/Middleware/ParticipantStatusDeleteValidator.php
@@ -5,6 +5,7 @@ namespace Middleware;
 use AllDigitalRewards\StatusEnum\StatusEnum;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Services\Participant\ServiceFactory;
 use Slim\Http\Response;
 
 class ParticipantStatusDeleteValidator
@@ -14,9 +15,15 @@ class ParticipantStatusDeleteValidator
      */
     private $container;
 
+    /**
+     * @var ServiceFactory
+     */
+    private $participantService;
+
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
+        $this->participantService = $this->container->get('participant');
     }
 
     /**
@@ -31,10 +38,30 @@ class ParticipantStatusDeleteValidator
         callable $next = null
     ) {
         $status = $request->getParsedBody()['status'] ?? '';
-        if ((new StatusEnum())->hydrateStatus($status) === StatusEnum::DATADEL) {
+        if ($this->hasDeleteStatus($status)) {
             return $response->withJson(['message' => _('DATADEL can only be set by DELETING the participant.')], 400);
         }
 
+        //Prevent any updates to a Participant IF DATADEL is their status
+        if ($request->getMethod() === 'PUT') {
+            $args = ($request->getAttribute('route'))->getArguments();
+            $participant = $this->participantService->getService()->getSingle($args['id']);
+            if ($this->hasDeleteStatus($participant->getStatus())) {
+                return $response->withJson(
+                    [
+                        'message' => _('Participant status is DATADEL, no further updates can be made.')
+                    ],
+                    400
+                );
+            }
+        }
+
+
         return $next($request, $response);
+    }
+
+    private function hasDeleteStatus($status)
+    {
+        return (new StatusEnum())->hydrateStatus($status) === StatusEnum::DATADEL;
     }
 }
