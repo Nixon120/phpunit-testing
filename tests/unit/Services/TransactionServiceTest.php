09911ecb5c49c667c75caee1caf26c735963ee2f
admin <admin@admins-MacBook-Pro.local>
2021-03-25 14:42:44 -0500

    DS-2603 add a test

--- tests/unit/Services/TransactionServiceTest.php
+++ tests/unit/Services/TransactionServiceTest.php
@@ -1,10 +1,35 @@
 <?php
 
+use AllDigitalRewards\AMQP\MessagePublisher;
+use Entities\Participant;
+use PHPUnit\Framework\TestCase;
+use Repositories\BalanceRepository;
+use Repositories\ParticipantRepository;
+use Repositories\TransactionRepository;
+use Services\Participant\Transaction;
 
-namespace unit\Services;
-
-
-class TransactionServiceTest
+class TransactionServiceTest extends TestCase
 {
-
+    public function testInsertParticipantTransactionWithNoProductsReturnsNull()
+    {
+        $mockTransactionRepository = $this->createMock(TransactionRepository::class);
+        $mockParticipantRepository = $this->createMock(ParticipantRepository::class);
+        $mockBalanceRepository = $this->createMock(BalanceRepository::class);
+        $mockMessagePublisher = $this->createMock(MessagePublisher::class);
+        $transactionService = new Transaction(
+            $mockTransactionRepository,
+            $mockParticipantRepository,
+            $mockBalanceRepository,
+            $mockMessagePublisher
+        );
+        $participant = new Participant();
+        $participant->setId(1);
+        $participant->setUniqueId('joesuuid');
+        $data['products'] = ['VVISA01', 'VVISA02'];
+        $response = $transactionService->insertParticipantTransaction($participant, []);
+        $error = ['No Products were included in transaction.'];
+        $mockTransactionRepository->method('setErrors')
+            ->with($error);
+        $this->assertSame(null, $response);
+    }
 }
